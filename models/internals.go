6aabbe2b5e10f359c50e2d9c63d33c8e0f42cb9d
alex breadman <3dukltd@gmail.com>
2023-11-28 20:59:01 +0000

    auto

--- models/internals.go
+++ models/internals.go
@@ -41,31 +41,18 @@ type Internals struct {
 
 func (i *Internals) DocPath() string {
 	p := strings.Split(string(i.ID[1:]), ".")
-	println(len(p))
-
 	parts := make([][]string, len(p))
-
 	k := ""
 	for x, s := range p {
 		k += "." + s
-		println("K", s)
 		parts[x] = strings.Split(k, ".")
-		println(strings.Join(parts[x], "."))
 
 	}
-
 	outs := []string{}
 	for _, p := range parts {
-
 		class := strings.Split(p[len(p)-1], "-")[0]
-
 		outs = append(outs, class+"/"+strings.Join(p, "."))
 	}
-
-	for _, o := range outs {
-		println("OUT   ", o)
-	}
-
 	return strings.Join(outs, "/")
 }
 
