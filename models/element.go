2ab18657d0a13d618c719c1efd854eb673c1db29
alex breadman <3dukltd@gmail.com>
2023-11-29 17:16:40 +0000

    auto

--- models/element.go
+++ models/element.go
@@ -1,15 +1,18 @@
 package models
 
+import "github.com/google/uuid"
+
 type Element struct {
 	Meta  Internals
 	Name  string `json:"name" firestore:"name"`
 	Image string `json:"image" firestore:"image"`
 }
 
-func (layer *Layer) NewElement(name string, options *LayerOptions) *Element {
+func (layer *Layer) NewElement(name string) *Element {
 	c := &Element{
-		Meta: layer.Meta.NewInternals("elements"),
-		Name: name,
+		Meta:  layer.Meta.NewInternals("elements"),
+		Name:  name,
+		Image: uuid.NewString(),
 	}
 	return c
 }
