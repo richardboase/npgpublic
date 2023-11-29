4951ddc76511d69554c1c2b0804141f520eef38b
alex breadman <3dukltd@gmail.com>
2023-11-29 21:15:21 +0000

    auto

--- models/tag.go
+++ models/tag.go
@@ -1,25 +1,28 @@
 package models
 
 type Tag struct {
-	Meta  Internals
-	Name  string `json:"name" firestore:"name"`
-	Color string `json:"color" firestore:"color"`
+	Meta            Internals
+	Name            string `json:"name" firestore:"name"`
+	BackgroundColor string `json:"backgroundColor" firestore:"backgroundColor"`
+	TextColor       string `json:"textColor" firestore:"textColor"`
 }
 
-func (collection *Collection) NewTag(name, color string) *Tag {
+func (collection *Collection) NewTag(name, backgroundColor, textColor string) *Tag {
 	c := &Tag{
-		Meta:  collection.Meta.NewInternals("tags"),
-		Name:  name,
-		Color: color,
+		Meta:            collection.Meta.NewInternals("tags"),
+		Name:            name,
+		BackgroundColor: backgroundColor,
+		TextColor:       textColor,
 	}
 	return c
 }
 
-func (element *Element) NewTag(name, color string) *Tag {
+func (element *Element) NewTag(name, backgroundColor, textColor string) *Tag {
 	c := &Tag{
-		Meta:  element.Meta.NewInternals("tags"),
-		Name:  name,
-		Color: color,
+		Meta:            element.Meta.NewInternals("tags"),
+		Name:            name,
+		BackgroundColor: backgroundColor,
+		TextColor:       textColor,
 	}
 	return c
 }
