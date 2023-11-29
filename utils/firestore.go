a5e055aa04d61187b686321c11d02727394179a6
alex breadman <3dukltd@gmail.com>
2023-11-29 23:49:58 +0000

    auto

--- utils/firestore.go
+++ utils/firestore.go
@@ -21,30 +21,17 @@ func GetDocument(app *common.App, id string, dst interface{}) error {
 
 func FirestorePath(id string) string {
 	p := strings.Split(string(id[1:]), ".")
-	println(len(p))
-
 	parts := make([][]string, len(p))
-
 	k := ""
 	for x, s := range p {
 		k += "." + s
-		println("K", s)
 		parts[x] = strings.Split(k, ".")
-		println(strings.Join(parts[x], "."))
 
 	}
-
 	outs := []string{}
 	for _, p := range parts {
-
 		class := strings.Split(p[len(p)-1], "-")[0]
-
 		outs = append(outs, class+"/"+strings.Join(p, "."))
 	}
-
-	for _, o := range outs {
-		println("OUT   ", o)
-	}
-
 	return strings.Join(outs, "/")
 }
