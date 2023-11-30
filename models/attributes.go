e39efd0a1a9bdf9e814c97cdf985a72a7682c0fe
alex breadman <3dukltd@gmail.com>
2023-11-30 21:41:55 +0000

    auto

--- models/attributes.go
+++ models/attributes.go
@@ -1,22 +1,43 @@
 package models
 
+import "net/http"
+
 type Attribute struct {
-	Meta  Internals
-	Name  string `json:"name" firestore:"name"`
-	Min   int    `json:"min" firestore:"min"`
-	Max   int    `json:"max" firestore:"max"`
-	Value int    `json:"value" firestore:"value"`
+	Meta    Internals
+	Name    string           `json:"name" firestore:"name"`
+	Options AttributeOptions `json:"options" firestore:"options"`
+	Value   int              `json:"value" firestore:"value"`
 }
 
-func (collection *Collection) NewAttribute(name string, min, max int) *Attribute {
+func (collection *Collection) NewAttribute(name string, options AttributeOptions) *Attribute {
 	c := &Attribute{
-		Meta: collection.Meta.NewInternals("attributes"),
-		Name: name,
-		Min:  min,
-		Max:  max,
+		Meta:    collection.Meta.NewInternals("attributes"),
+		Name:    name,
+		Options: options,
 	}
 	return c
 }
 
 type AttributeOptions struct {
+	Min int
+	Max int
+}
+
+func (attribute *Attribute) ValidateInput(w http.ResponseWriter, m map[string]interface{}) bool {
+
+	var exists bool
+
+	attribute.Name, exists = AssertKeyValue(w, m, "name")
+	if !exists {
+		return false
+	}
+
+	attribute.Options.Min, exists = AssertKeyValueInt(w, m, "min")
+	if !exists {
+		return false
+	}
+
+	attribute.Options.Max, exists = AssertKeyValueInt(w, m, "max")
+
+	return exists
 }
