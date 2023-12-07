a60604a83320cdc111f1b057eb9f30c9d2acf953
alex breadman <3dukltd@gmail.com>
2023-12-07 15:41:29 +0000

    auto

--- models/job.go
+++ models/job.go
@@ -8,11 +8,11 @@ import (
 
 type Job struct {
 	Meta    Internals
-	Name    string   `json:"name" firestore:"name"`
-	Status  string   `json:"status" firestore:"status"`
-	Jobs    []string `json:"subjobs" firestore:"subjobs"`
-	SubJobs map[string]*Job
-	JSON    string
+	Name    string          `json:"name" firestore:"name"`
+	Status  string          `json:"status" firestore:"status"`
+	Jobs    []string        `json:"jobs" firestore:"jobs"`
+	SubJobs map[string]*Job `json:"subjobs" firestore:"subjobs"`
+	JSON    string          `json:"json" firestore:"json"`
 }
 
 func (job *Job) NewJob(name, status string) *Job {
