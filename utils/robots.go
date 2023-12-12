69d17562bb1106dc583e305cc8d984a60e11d4c6
Echo <echo@Echos-MacBook-Pro.local>
2023-12-12 16:39:40 +0000

    auto

--- utils/robots.go
+++ utils/robots.go
@@ -1,1 +1,40 @@
 package utils
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/go-resty/resty/v2"
+)
+
+func getRobotURL(target string, query *url.Values) (string, error) {
+
+	host := "https://europe-west2-ninja-punk-girls.cloudfunctions.net/"
+
+	switch target {
+	case "prepare":
+		host += "robot-prepare"
+
+	default:
+		return "", fmt.Errorf("no switch case found for %s", target)
+	}
+
+	return host + "?" + query.Encode(), nil
+}
+
+func CallRobot(target string, query *url.Values, dst interface{}) error {
+
+	host, err := getRobotURL(target, query)
+	if err != nil {
+		return err
+	}
+
+	client := resty.New()
+
+	_, err = client.R().SetResult(dst).Get(host)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
