a0e79a549a7fdee4acff78a3348d163ed065cc09
Echo <echo@echos-mbp.home>
2023-12-18 22:17:46 +0000

    auto

--- models/design.go
+++ models/design.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type Design struct {
 	Meta       Internals
 	Index      int               `json:"index" firestore:"index"`
@@ -13,5 +15,6 @@ func (job *Job) NewDesign(index int) *Design {
 		Meta:  job.Meta.NewInternals("designs"),
 		Index: index,
 	}
+	c.Meta.ID = fmt.Sprintf(".%s-%d", c.Meta.Class, index)
 	return c
 }
