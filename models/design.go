e9db62652001a559b46bd30260dbf0407c263e3b
Echo <echo@echos-mbp.home>
2023-12-18 22:11:22 +0000

    auto

--- models/design.go
+++ models/design.go
@@ -2,13 +2,13 @@ package models
 
 type Design struct {
 	Meta       Internals
-	Index      string            `json:"index" firestore:"index"`
+	Index      int               `json:"index" firestore:"index"`
 	Name       string            `json:"name" firestore:"name"`
 	Attributes []*Attribute      `json:"attributes" firestore:"attributes"`
 	Data       map[string]string `json:"data" firestore:"data"`
 }
 
-func (job *Job) NewDesign(index string) *Design {
+func (job *Job) NewDesign(index int) *Design {
 	c := &Design{
 		Meta:  job.Meta.NewInternals("designs"),
 		Index: index,
