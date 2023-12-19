531b95580f841c8be24552ce74ba7f3c9513e81e
Echo <echo@echos-mbp.home>
2023-12-19 16:20:30 +0000

    auto

--- models/overlay.go
+++ models/overlay.go
@@ -2,16 +2,15 @@ package models
 
 type Overlay struct {
 	Meta    Internals
-	Name    string `json:"name" firestore:"name"`
 	Type    string `json:"type" firestore:"type"`
 	Content string `json:"content" firestore:"content"`
 	X       int    `json:"x" firestore:"x"`
 	Y       int    `json:"y" firestore:"y"`
 }
 
-func (collection *Collection) NewOverlay(overlayType, content string, x, y int) *Overlay {
+func (layer *Layer) NewOverlay(overlayType, content string, x, y int) *Overlay {
 	c := &Overlay{
-		Meta: collection.Meta.NewInternals("layers"),
+		Meta: layer.Meta.NewInternals("overlays"),
 		Type: overlayType,
 		X:    x,
 		Y:    y,
